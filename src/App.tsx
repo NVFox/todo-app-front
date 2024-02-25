93e20d71585e4ca7ba8fcb75a26455f9c92fddd9
ADT64 <davidroa8@hotmail.com>
2024-02-25 03:28:50 -0500

    feat(task): add toast to improve ux

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Home } from './pages/home/Home'
+import { Toaster } from './components/ui/toaster'
 
 const queryClient = new QueryClient()
 
@@ -9,6 +10,7 @@ function App() {
       <QueryClientProvider client={queryClient}>
         <Home />
       </QueryClientProvider>
+      <Toaster />
     </>
   )
 }
--- src/hooks/task.hook.ts
+++ src/hooks/task.hook.ts
@@ -3,6 +3,7 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { TaskService } from "@/services/task.service";
 import { TaskMapper } from "@/mappers/task.mapper";
 import { Page, PageRequestDto } from "@/dto/page.dto";
+import { useToast } from "@/components/ui/use-toast";
 
 const taskMapper = new TaskMapper();
 const taskService = new TaskService(taskMapper);
@@ -16,17 +17,32 @@ export const useAllTasks = (pageRequest?: PageRequestDto) => {
 
 export const useCreateTask = () => {
   const queryClient = useQueryClient();
+  const { toast } = useToast();
 
   return useMutation({
     mutationFn: (task: Task) => taskService.createTask(taskMapper.taskToCreateUpdateDto(task)),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["todos"] })
+    },
+    onSettled: (_data, error) => {
+      const { dismiss } = toast({
+        variant: error ? "destructive": "default",
+        title: error ? error.message : "La tarea se ha creado con éxito.",
+        description: error ? "Algo salió mal." : "Ya puedes visualizarla en el menú principal"
+      })
+
+      const timeoutId = setTimeout(() => {
+        dismiss()
+      }, 100)
+
+      clearTimeout(timeoutId);
     }
   })
 }
 
 export const useUpdateTask = () => {
   const queryClient = useQueryClient();
+  const { toast } = useToast();
 
   return useMutation({
     mutationFn: (task: Task) => taskService.updateTask(task.id ?? 0, taskMapper.taskToCreateUpdateDto(task)),
@@ -37,7 +53,19 @@ export const useUpdateTask = () => {
         })
       })
     },
-    onSettled: () => {
+    onSettled: (_data, error) => {
+      const { dismiss } = toast({
+        variant: error ? "destructive": "default",
+        title: error ? error.message : "La tarea se ha actualizado con éxito.",
+        description: error ? "Algo salió mal." : "Ya puedes visualizarla en el menú principal"
+      })
+
+      const timeoutId = setTimeout(() => {
+        dismiss()
+      }, 100)
+
+      clearTimeout(timeoutId);
+
       queryClient.invalidateQueries({ queryKey: ["todos"] })
     },
   })
@@ -45,6 +73,7 @@ export const useUpdateTask = () => {
 
 export const useDeleteTask = () => {
   const queryClient = useQueryClient();
+  const { toast } = useToast();
 
   return useMutation({
     mutationFn: (id: number) => taskService.deleteTask(id),
@@ -53,7 +82,19 @@ export const useDeleteTask = () => {
         return old.filter(item => item.id === id)
       })
     },
-    onSettled: () => {
+    onSettled: (_data, error) => {
+      const { dismiss } = toast({
+        variant: error ? "destructive": "default",
+        title: error ? error.message : "La tarea se ha eliminado con éxito.",
+        description: error ? "Algo salió mal." : "Ya puedes visualizar este cambio en el menú principal"
+      })
+
+      const timeoutId = setTimeout(() => {
+        dismiss()
+      }, 100)
+
+      clearTimeout(timeoutId);
+
       queryClient.invalidateQueries({ queryKey: ["todos"] })
     },
   })
